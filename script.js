d7c9523294aecfd028f323deafecfed35b9cdb97
Diego <131261339+DiegoNoxe@users.noreply.github.com>
2024-06-15 19:33:30 -0300

    Mudanças
    Melhorando a Logica do Js.
    Alterando Cores no Css.
    Adicionando Animação no Texto.

--- script.js
+++ script.js
@@ -4,36 +4,41 @@ function resultado()
         idade      = document.getElementById("idade").value;
         altura     = parseFloat(document.getElementById("altura").value);
         peso       = parseFloat(document.getElementById("peso").value);
-        imc = (peso/(altura*altura)).toFixed(2);
-        
         resultado = "";
 
+
+    
+        if (altura > 100) {
+            altura = altura / 100;
+        }
+        imc = (peso/(altura*altura)).toFixed(2);
+
         if(idade >= 18){
-            if(imc < 18.5){
+            if(imc <= 18.5){
                 resultado = `Seu peso é ${peso}, mas sua idade é ${idade}, você está abaixo do peso ideal! Seu imc: ${imc}`;
             }
 
             
-            else if(imc > 18.8 && imc < 24.9){
+            else if(imc >= 18.6 && imc <= 24.9){
                 resultado = `Seu peso é ${peso} e sua idade é ${idade}, você está no peso normal! Seu imc: ${imc}`;
             }
 
             
-            else if(imc > 25 && imc < 29.9){
+            else if(imc >= 25 && imc <= 29.9){
                 resultado = `Seu peso é ${peso} e a sua idade é ${idade}, você está com Sobrepeso Seu imc: ${imc}`;
             }
 
             
-            else if(imc > 30 && imc < 34.9){
+            else if(imc >= 30 && imc <= 34.9){
                 resultado = `Seu peso é ${peso}, mas sua idade é ${idade}, você está com Obesidade Grau 1! Seu imc: ${imc}`;
             }
 
             
-            else if(imc > 35 && imc < 39.9){
+            else if(imc >= 35 && imc <= 39.9){
                 resultado = `Seu peso é ${peso}, mas sua idade é ${idade}, você está com Obesidade Grau 2! Seu imc: ${imc}`;
             }
 
-            else if(imc > 40){
+            else if(imc >= 40){
                 resultado = `Seu peso é ${peso}, mas sua idade é ${idade}, você está com Obesidade Grau III (Obesidade Mórbida)! Seu imc: ${imc}`;
             }
         }
